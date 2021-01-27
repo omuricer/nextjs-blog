4b39d170746f102e9a076905add8f87ea91a9ecd
omuricer <guts.inaq@gmail.com>
2021-01-27 17:33:22 +0900

    add comment

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,6 +24,7 @@ export default function Home({ allPostsData }) {
       </Head>
       <section className={utilStyles.headingMd}>
         <p>[Your Self Introduction]</p>
+        <p>Branch sample.</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{" "}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
