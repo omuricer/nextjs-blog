d2e198c82151d4bd05b8949745c38ab19d2fe27f
omuricer <guts.inaq@gmail.com>
2021-01-28 00:54:52 +0900

    change path to absolute

--- next.config.js
+++ next.config.js
@@ -1,9 +1,8 @@
 const path = require("path");
-module.exports = (phase, { defaultConfig }) => {
-  return {
-    webpack(config, options) {
-      config.resolve.alias["@"] = path.join(__dirname, "src");
-      return config;
-    },
-  };
+
+module.exports = {
+  webpack(config, options) {
+    // config.resolve.alias["@"] = path.join(__dirname, "src");
+    return config;
+  },
 };
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import App, { AppProps } from "next/app";
-import "@/styles/global.css";
+import "styles/global.css";
 
 const MyApp: React.FC<AppProps> = (props) => {
   return <props.Component {...props.pageProps} />;
